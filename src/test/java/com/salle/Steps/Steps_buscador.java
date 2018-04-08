9f27308d020f6aaf43af0eaf42dc75045f61fe0f
DESKTOP-BSD5S3P\ivanm <ivamolmal@gmail.com>
2018-04-08 11:12:35 +0200

    Test buscador right y wrong Nueva version de chrome driver

--- src/test/java/com/salle/Steps/Steps_buscador.java
+++ src/test/java/com/salle/Steps/Steps_buscador.java
@@ -2,7 +2,6 @@ package com.salle.Steps;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -12,7 +11,6 @@ public class Steps_buscador {
 	
 	public WebDriver driver;
 	public Pantalla_Home ph;
-	Actions actions = new Actions(driver);
 	
 	public Steps_buscador(WebDriver driver) {
 		this.driver = driver;
@@ -23,23 +21,44 @@ public class Steps_buscador {
 	public void Despliega_buscador(WebDriver driver) {
 		try {
 			ph.getbuscador().click();
-			WebDriverWait wait = new WebDriverWait(driver, 10);
-			wait.until(ExpectedConditions.presenceOfElementLocated(By.id("edit-search-block-form--2")));	
+			
 		}catch(Exception e) {
 			System.out.println("Error al clicar en buscador");
 		}
+		
+		WebDriverWait wait = new WebDriverWait(driver, 10);
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.id("edit-search-block-form--2")));	
 	}
 	
 	public void Escriu_buscador(WebDriver driver) {
 		try {
 			driver.findElement(By.id("edit-search-block-form--2")).click();
-			//actions.moveToElement();
-			driver.findElement(By.id("search-block-form")).sendKeys("chipirones");
-			driver.findElement(By.id("edit-submit--2")).click();
+			driver.findElement(By.xpath("//*[@id=\"edit-search-block-form--2\"]")).sendKeys("David Vernet");
+			driver.findElement(By.id("edit-submit--3")).click();
+				
 		}catch(Exception e) {
 			System.out.println("Error al escriure en el buscador");
 			System.out.println(e);
 		}
+		
+		WebDriverWait wait = new WebDriverWait(driver, 20);
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("/html/body/div[5]/div[1]/div/div/section/div/div[2]/div[3]/div/div/ol/li[2]/h3/a")));	
+	}
+	
+	public void Escriu_buscador_wrong(WebDriver driver) {
+		try {
+			driver.findElement(By.id("edit-search-block-form--2")).click();
+			driver.findElement(By.xpath("//*[@id=\"edit-search-block-form--2\"]")).sendKeys("Chipirones");
+			driver.findElement(By.id("edit-submit--2")).click();
+				
+		}catch(Exception e) {
+			System.out.println("Error al escriure en el buscador");
+			System.out.println(e);
+		}
+		
+		WebDriverWait wait = new WebDriverWait(driver, 10);
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("/html/body/div[5]/div[1]/div/div/section/div/div[2]/div[3]/div/div/ol/li[2]/h3/a")));	
+		driver.findElements(By.tagName("Chipirones"));	
 	}
 	
 }
--- src/test/java/com/salle/test/funcionales/header/Test_buscador.java
+++ src/test/java/com/salle/test/funcionales/header/Test_buscador.java
@@ -2,12 +2,11 @@ package com.salle.test.funcionales.header;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import com.salle.Steps.Steps_buscador;
-import com.salle.Steps.Steps_header;
 import com.salle.pantallas.Pantalla_Home;
 
 public class Test_buscador {
@@ -16,12 +15,12 @@ public class Test_buscador {
 	public Steps_buscador sb = new Steps_buscador(driver);
 	public Pantalla_Home ph = new Pantalla_Home(driver);
 	
-	 @BeforeTest
+	 @BeforeClass
 	  public void beforeClass() {
 		  ph.getSalleUrl(driver);
 	  }
 	
-	  @AfterTest(alwaysRun = true)
+	  @AfterClass
 	  public void afterClass() {
 		  driver.quit();
 	  }
@@ -29,11 +28,15 @@ public class Test_buscador {
 	  @Test(priority = 0)
 	  public void Test_negativo() {
 		  sb.Despliega_buscador(driver);
-		  sb.Escriu_buscador(driver);
+		  sb.Escriu_buscador_wrong(driver);
+		  
 	  }
+	  
 	  @Test(priority = 1)
 	  public void Test_positivo() {
-		  
+		  sb.Despliega_buscador(driver);
+		  sb.Escriu_buscador(driver);
+		 
 	  }
 	  
 	
